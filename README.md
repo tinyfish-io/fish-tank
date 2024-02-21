fe563b8be347ec952d02ff8ff4c38786531169a7
Urvish Thakker <urvishthakker@Urvishs-MacBook-Pro.local>
2024-02-21 23:17:52 +0530

    Add first FishTank example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# empty-repo
+# A repository to house AgentQL Examples and tutorials
+
+
+| Topic | Example Name | URL |
+| -------- | -------- | -------- |
+| How to Login | --   | --   |
+
---
+++ examples/customizing_web_driver/customizing_web_driver.py
@@ -0,0 +1,21 @@
+"""This example demonstrates how to customize the web driver used by AgentQL."""
+import time
+import webql
+
+# Importing the default PlaywrightWebDriver from AgentQL library
+from webql.web import PlaywrightWebDriver
+
+# Set the URL to the desired website
+URL = "https://www.google.com"
+
+if __name__ == "__main__":
+
+    # Set headless to False to see the browser in action
+    driver = PlaywrightWebDriver(headless=False)
+
+    # Start a session with the specified URL and the custom driver
+    session = webql.start_session(URL, web_driver=driver)
+
+    # Wait for 5 seconds to see the browser in action
+    time.sleep(5)
+
