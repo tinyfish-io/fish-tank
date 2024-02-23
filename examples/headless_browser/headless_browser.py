1db906aea99a81183ba3b100c7eeb738186d703c
thakkerurvish <141964033+thakkerurvish@users.noreply.github.com>
2024-02-23 09:59:02 -0800

    Headless Browser (#5)
    * Headless Browser
    
    * Update headless_browser.py
    
    ---------
    
    Co-authored-by: Urvish Thakker <urvishthakker@Urvishs-MacBook-Pro.local>

---
+++ examples/headless_browser/headless_browser.py
@@ -0,0 +1,36 @@
+"""This example demonstrates how to work with default web driver configuration (headless) used by AgentQL."""
+import webql
+
+# Set the URL to the desired website
+URL = "https://google.com"
+
+if __name__ == "__main__":
+
+    QUERY = """
+    {
+        search_input
+        search_btn
+    }"""
+    
+    # Start a session with the specified URL and the custom driver
+    session = webql.start_session(URL)
+
+    response = session.query(QUERY)
+
+    response.search_input.fill("President of United States")
+
+    response.search_btn.click(force=True)
+
+    QUERY = """
+    {
+        urls[]
+        {
+            link
+        }
+    }"""
+
+    response = session.query(QUERY)
+
+    print(response)
+
+
