3b82a4f8164db5185d1820c0dfc1f0bcaa57bab2
thakkerurvish <141964033+thakkerurvish@users.noreply.github.com>
2024-02-28 07:30:31 +0530

    Wait for load state usage script (#8)
    * Wait for load state usage script
    
    * Update wait_for_entire_page_load.py
    
    ---------
    
    Co-authored-by: Urvish Thakker <urvishthakker@Urvishs-MacBook-Pro.local>

---
+++ examples/wait_for_entire_page_load/wait_for_entire_page_load.py
@@ -0,0 +1,39 @@
+"""This example demonstrates how to wait for the page to load before querying the page and levergae scroll method."""
+import webql
+
+from webql.sync_api.web import PlaywrightWebDriver
+
+# Yotube video URL to demonstrate the example for loading comments on the video
+URL = "https://www.youtube.com/watch?v=F6hmwkI3n64"
+
+if __name__ == "__main__":
+    # Set headless to False to see the browser in action
+    driver = PlaywrightWebDriver(headless=False)
+
+    # Start a session with the specified URL and the custom driver
+    session = webql.start_session(URL, web_driver=driver)
+
+    # (Note: The current script is configured to load about 100 comments on the video)
+    for i in range(5):
+        #  Each Scroll will load about 20 comments on the video
+        driver.scroll_to_bottom()
+
+        # Wait for the page to load (helps to load the comments on the video)
+        driver.wait_for_page_ready_state()
+
+    QUERY = """
+    {
+        comments[] {
+            comment_title
+            author
+            date
+        }
+    }
+    """
+
+    response = session.query(QUERY)
+
+    # TODO: Add appropriate code for consumtpion of the comments data
+    # Example: Levergae AgentQL's to_data() method to extract the content of comments and do sentiment analysis of the comments on the video
+    
+    session.stop()
