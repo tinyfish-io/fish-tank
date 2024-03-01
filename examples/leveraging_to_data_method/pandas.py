3fd3e8c33ad076c399c89c92cded92e03c9091f7
Justin Woo <code.jwoo@gmail.com>
2024-03-01 07:42:13 -0800

    Add pandas example (#12)
    * Create pandas.py
    
    * Update pandas.py
    
    * Rename application_example/pandas.py to examples/leveraging_to_data_method/pandas.py
    
    * Create nikeTable.txt
    
    * Create nike.json
    
    * Update examples/leveraging_to_data_method/pandas.py
    
    Co-authored-by: thakkerurvish <141964033+thakkerurvish@users.noreply.github.com>
    
    * Update examples/leveraging_to_data_method/pandas.py
    
    Co-authored-by: thakkerurvish <141964033+thakkerurvish@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: thakkerurvish <141964033+thakkerurvish@users.noreply.github.com>

---
+++ examples/leveraging_to_data_method/pandas.py
@@ -0,0 +1,48 @@
+This example scrapes prices from nike.com using a simple query and the to_data method. It then uses pandas to output a table to a file
+# It then uses pandas to output a table to a file
+
+
+import webql
+import json
+import pandas as pd
+import logging
+from webql.sync_api.web import PlaywrightWebDriver
+
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+URL = "https://www.nike.com/w/mens-shoes-nik1zy7ok"
+
+driver = PlaywrightWebDriver(headless=False)
+session = webql.start_session(URL, web_driver=driver)
+
+QUERY = """
+{
+    shoes[]{
+        title
+        price
+        colors
+    }
+    
+}
+"""
+response = session.query(QUERY)
+data = response.shoes.to_data()
+
+#Let's dump the data out to a json file for us to use.
+
+with open("nike.json", "w") as outfile:
+    json.dump(data, outfile)
+
+#We use pandas to take the dictand convert it to a dataframe
+df = pd.DataFrame.from_dict(data)
+
+#This makes it super simple to print a table
+print(df.to_string())
+
+#We can even write that table to a file                                                                  
+with open('nikeTable.txt', 'a') as f:
+    f.write(df.to_string())
+
+session.stop()
