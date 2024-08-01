afc18e871d4886806bfab0ca4b09bd36861ecff2
Shuhao Zhang <shuhao@tinyfish.io>
2024-08-01 14:33:06 -0700

    use page apis

--- examples/ecommerce_pricing_data/pricing_data.py
+++ examples/ecommerce_pricing_data/pricing_data.py
@@ -4,19 +4,19 @@
 """
 
 import asyncio
-import agentql
-from agentql.async_api import Session
+from playwright.async_api import async_playwright
+from agentql.ext.playwright.async_api import Page
 
 # URL of the ecommerce website
 # You can replace it with any other ecommerce website but the queries should be updated accordingly
 URL = "https://www.bestbuy.com"
 
 
-async def _do_extract_pricing_data(session: Session) -> list:
+async def _do_extract_pricing_data(page: Page) -> list:
     """Extract pricing data from the current page.
 
     Args:
-        session (Session): The AgentQL session object to interact with the browser.
+        page (Page): The Playwright page object to interact with the browser.
 
     Returns:
         list: The pricing data extracted from the page.
@@ -31,14 +31,13 @@ async def _do_extract_pricing_data(session: Session) -> list:
             price
         }
     }"""
-    pricing_data = await session.query_data(query)
-    pricing_data = await pricing_data.to_data()
+    pricing_data = await page.query_data(query)
 
     return pricing_data.get("products", [])
 
 
 async def _search_product(
-    session: Session,
+    page: Page,
     product: str,
     min_price: int,
     max_price: int,
@@ -46,46 +45,30 @@ async def _search_product(
     """Search for a product with a price range.
 
     Args:
-        session (Session): The AgentQL session object to interact with the browser.
+        page (Page): The Playwright page object to interact with the browser.
         product (str): The product name to search for.
         min_price (int): The minimum price of the product.
         max_price (int): The maximum price of the product.
     """
 
-    # Find the search field using smart locator
-    search_input_query = """
-    {
-        search_input
-    }"""
-
-    response = await session.query(search_input_query)
-    await response.search_input.type(product, delay=200)
-    await response.search_input.press("Enter")
-
-    # Wait for the search result to load
-    await session.driver.wait_for_page_ready_state()
+    # Search for a product
+    search_input = await page.get_by_prompt("the search input field")
+    await search_input.type(product, delay=200)
+    await search_input.press("Enter")
 
     # Define price range
-    price_range_query = """
-    {
-        min_price_input
-        max_price_input
-    }"""
-
-    response = await session.query(price_range_query)
-    await response.min_price_input.fill(str(min_price))
-    await response.max_price_input.fill(str(max_price))
-    await response.max_price_input.press("Enter")
-
-    # Wait for the search result to update
-    await session.driver.wait_for_page_ready_state()
+    min_price_input = await page.get_by_prompt("the min price input field")
+    await min_price_input.fill(str(min_price))
+    max_price_input = await page.get_by_prompt("the max price input field")
+    await max_price_input.fill(str(max_price))
+    await max_price_input.press("Enter")
 
 
-async def _go_to_the_next_page(session: Session) -> bool:
+async def _go_to_the_next_page(page: Page) -> bool:
     """Navigate to the next page of the search results.
 
     Args:
-        session (Session): The AgentQL session object to interact with the browser.
+        page (Page): The Playwright page object to interact with the browser.
 
     Returns:
         bool: True if the next page is navigated successfully, False if no more next page.
@@ -99,16 +82,14 @@ async def _go_to_the_next_page(session: Session) -> bool:
         }
     }"""
     print("Navigating to the next page...")
-    response = await session.query_data(next_page_query)
-    pagination = await response.to_data()
+    pagination = await page.query_data(next_page_query)
     next_page_url = pagination.get("pagination", {}).get("next_page_url")
     if not next_page_url:
         return False
     try:
         if not next_page_url.startswith("http"):
             next_page_url = URL + next_page_url  # Make it a full URL
-        await session.driver.open_url(next_page_url)
-        await session.driver.wait_for_page_ready_state()
+        await page.goto(next_page_url)
         return True
     except Exception:
         pass
@@ -117,52 +98,58 @@ async def _go_to_the_next_page(session: Session) -> bool:
 
 
 async def extract_pricing_data(
+    page: Page,
     product: str,
     min_price: int,
     max_price: int,
     max_pages: int = 3,
 ) -> list:
     """Extract pricing data for a product within a price range."""
-
-    # Start an async browser session with the specified URL
-    session = await agentql.start_async_session(URL)
-
     # Search for the product with the specified price range
     print(
-        f"Searching for product: ${product} with price range: ${min_price} - ${max_price}"
+        f"Searching for product: {product} with price range: ${min_price} - ${max_price}"
     )
-    await _search_product(session, product, min_price, max_price)
+    await _search_product(page, product, min_price, max_price)
 
     current_page = 1
     pricing_data = []
     while current_page <= max_pages:
         # Extract pricing data from the current page
         print(f"Extracting pricing data on page {current_page}...")
-        pricing_data_on_page = await _do_extract_pricing_data(session)
-        print(f"{len(pricing_data_on_page)} pricing data found")
+        pricing_data_on_page = await _do_extract_pricing_data(page)
+        print(f"{len(pricing_data_on_page)} products found")
 
         pricing_data.extend(pricing_data_on_page)
 
         # Navigate to the next page
-        if not await _go_to_the_next_page(session):
+        if not await _go_to_the_next_page(page):
             print("No more next page.")
             break
 
         current_page += 1
 
-    # Stop the browser session
-    await session.stop()
-
     return pricing_data
 
 
+async def main():
+    """Main function."""
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
+        page: Page = await browser.new_page()
+        await page.goto(URL)  # open the target URL
+
+        pricing_data = await extract_pricing_data(
+            page,
+            product="gpu",
+            min_price=500,
+            max_price=800,
+        )
+
+        print(pricing_data)
+
+        # Stop the browser session
+        await browser.close()
+
+
 if __name__ == "__main__":
-    # Define the product and price range to search for
-    PRODUCT = "gpu"
-    MIN_PRICE = 500
-    MAX_PRICE = 800
-
-    # Extract pricing data for the product within the price range
-    pricing_data = asyncio.run(extract_pricing_data(PRODUCT, MIN_PRICE, MAX_PRICE))
-
-    print(pricing_data)
+    asyncio.run(main())
