b40231c276ec76cff8a5b5f4bd377f3a0c007358
Tianling Feng <frank@tinyfish.io>
2024-08-07 18:34:55 -0500

    Fix login example

---
+++ examples/login/README.md
@@ -0,0 +1,16 @@
+# Example script: log into a website with AgentQL
+
+This example demonstrates how to log into a website using AgentQL.
+
+## Run the script
+
+* [Install AgentQL SDK](https://docs.agentql.com/docs/installation/sdk-installation)
+* Save this python file locally as **login.py**
+* Run the following command from the project's folder:
+```bash
+python3 login.py
+```
+
+## Play with it
+
+Install the [AgentQL Chrome DevTools extension](https://docs.agentql.com/docs/installation/chrome-extension-installation/) to play with the AgentQL query. [Learn more about the AgentQL query language](https://docs.agentql.com/docs/agentql-query/query-intro)
--- examples/login/login.py
+++ examples/login/login.py
@@ -1,61 +1,58 @@
-import time
-import agentql
+"""This example demonstrates how to log in to a website using AgentQL."""
 
-URL = "https://www.target.com"
+import time
+
+from agentql.ext.playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
+
+URL = "https://www.yelp.com/"
 EMAIL = "REPLACE_WITH_YOUR_EMAIL (For target.com)"
 PASSWORD = "REPLACE_WITH_YOUR_PASSWORD (For target.com)"
 
-if __name__ == "__main__":
+# Define the queries to interact with the page
+LOG_IN_QUERY = """
+{
+    log_in_btn
+}
+"""
 
-    # Start a session with the specified URL
-    session = agentql.start_session(URL)
-
-    # Define the queries to interact with the page
-    sign_in_query = """
-    {
-        sign_in_btn
-    }
-    """
-
-    credentials_query = """
-    {
+CREDENTIALS_QUERY = """
+{
+    sign_in_form {
         email_input
         password_input
-        sign_in_with_password_btn
+        log_in_btn
     }
-    """
-
-    skip_query = """
-    {
-        skip_btn
-    }
-    """
-
-    # Make API call(s) to AgentQL server to fetch the query
-    response = session.query(sign_in_query)
-
-    response.sign_in_btn.click(force=True)
-
-    response_sign_in = session.query(sign_in_query)
-
-    response_sign_in.sign_in_btn.click(force=True)
+}
+"""
 
 
-    response_credentials = session.query(credentials_query)
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
 
-    response_credentials.email_input.fill(EMAIL)
-    response_credentials.password_input.fill(PASSWORD)
-    response_credentials.sign_in_with_password_btn.click(force=True)
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = browser.new_page()
 
-    response_skip = session.query(skip_query)
+        page.goto(URL)
 
-    response_skip.skip_btn.click(force=True)
+        # Use query_elements() method to locate log in button on the page
+        response = page.query_elements(LOG_IN_QUERY)
+        # Click the log in button
+        response.log_in_btn.click(force=True)
 
-    response_skip_promos = session.query(skip_query)
+        # Use query_elements() method to locate email, password input fields, and log in button in sign-in form
+        response_credentials = page.query_elements(CREDENTIALS_QUERY)
+        # Fill the email and password input fields
+        response_credentials.sign_in_form.email_input.fill(EMAIL)
+        response_credentials.sign_in_form.password_input.fill(PASSWORD)
+        response_credentials.sign_in_form.log_in_btn.click(force=True)
 
-    response_skip_promos.skip_btn.click(force=True)
+        # Wait for 5 seconds to see the browser in action
+        time.sleep(5)
 
-    # Wait for 5 seconds to see the browser in action
-    time.sleep(5)
+        browser.close()
 
-    session.stop()
+
+if __name__ == "__main__":
+    main()
