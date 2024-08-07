4d1c1c894c67b46af314f6a9c783cbce446e6132
Tianling Feng <frank@tinyfish.io>
2024-08-07 14:48:16 -0500

    Update list query usage example

---
+++ examples/list_query_usage/README.md
@@ -0,0 +1,16 @@
+# Example script: querying a list of items with AgentQL
+
+This example demonstrates how to query a list of items on the page.
+
+## Run the script
+
+* [Install AgentQL SDK](https://docs.agentql.com/docs/installation/sdk-installation)
+* Save this python file locally as **list_query_usage.py**
+* Run the following command from the project's folder:
+```bash
+python3 list_query_usage.py
+```
+
+## Play with it
+
+Install the [AgentQL Chrome DevTools extension](https://docs.agentql.com/docs/installation/chrome-extension-installation/) to play with the AgentQL query. [Learn more about the AgentQL query language](https://docs.agentql.com/docs/agentql-query/query-intro)
--- examples/list_query_usage/list_query_usage.py
+++ examples/list_query_usage/list_query_usage.py
@@ -1,41 +1,50 @@
-import agentql
+"""This example demonstrates how to query a list of items on the page."""
+
 import os
+from playwright.sync_api import sync_playwright
+from agentql.ext.playwright.sync_api import Page
 
 # Set the URL to the desired website
 URL = "https://getyourfckingsocks.com/shop"
 
-if __name__ == "__main__":
-    
-    # Define the queries to interact with the page
-    QUERY = """
+# Define the queries to interact with the page
+QUERY = """
+{
+    socks[]
     {
-        socks[]
-        {
-            style_name
-            price
-        }
-    }"""
+        style_name
+        price
+    }
+}
+"""
 
-    # Start a session with the specified URL
-    session = agentql.start_session(URL)
 
-    # Make API call(s) to AgentQL server to fetch the query
-    response = session.query(QUERY)
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
 
-    response_data = response.to_data()
-    
-    #  Get the directory of the current script
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = browser.new_page()
 
-    # Create path to the csv file
-    csv_file_path = os.path.join(script_dir, "socks_data.csv")
+        page.goto(URL)
 
-    # Write the data to a csv file
-    with open(csv_file_path, "w") as file:
-        file.write("Style Name, Price\n")
-        for sock in response_data["socks"]:
-            file.write(f"{sock['style_name']},{sock['price']}\n")
-    
+        # Use query_data() method to fetch the data from the page
+        response = page.query_data(QUERY)
 
-    # Stop the session
-    session.stop()
+        #  Get the directory of the current script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Create path to the csv file
+        csv_file_path = os.path.join(script_dir, "socks_data.csv")
+
+        # Write the data to a csv file
+        with open(csv_file_path, "w", encoding="utf-8") as file:
+            file.write("Style Name, Price\n")
+            for sock in response["socks"]:
+                file.write(f"{sock['style_name']},{sock['price']}\n")
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
