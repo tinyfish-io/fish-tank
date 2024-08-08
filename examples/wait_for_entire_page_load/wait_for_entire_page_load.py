0a0e7f9d7b0ac80c798cb237c05b2130da20f311
Tianling Feng <frank@tinyfish.io>
2024-08-07 19:58:44 -0500

    Update wait for page load example

--- examples/wait_for_entire_page_load/wait_for_entire_page_load.py
+++ examples/wait_for_entire_page_load/wait_for_entire_page_load.py
@@ -1,35 +1,43 @@
-"""This example demonstrates how to wait for the page to load before querying the page and levergae scroll method."""
-import agentql
+"""This example demonstrates how to wait for the page to load completely before querying the page."""
+
+from agentql.ext.playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
 
 # Yotube video URL to demonstrate the example for loading comments on the video
 URL = "https://www.youtube.com/watch?v=F6hmwkI3n64"
 
-if __name__ == "__main__":
-    
-    # Start a session with the specified URL and the custom driver
-    session = agentql.start_session(URL)
-
-    # (Note: The current script is configured to load about 100 comments on the video)
-    for i in range(5):
-        #  Each Scroll will load about 20 comments on the video
-        session.driver.scroll_to_bottom()
-
-        # Wait for the page to load (helps to load the comments on the video)
-        session.driver.wait_for_page_ready_state()
-
-    QUERY = """
-    {
-        comments[] {
-            comment_title
-            author
-            date
-        }
+QUERY = """
+{
+    comments[] {
+        comment_content
+        author
+        date
     }
-    """
+}
+"""
 
-    response = session.query(QUERY)
 
-    # TODO: Add appropriate code for consumtpion of the comments data
-    # Example: Levergae AgentQL's to_data() method to extract the content of comments and do sentiment analysis of the comments on the video
-    print(response.to_data())
-    session.stop()
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = browser.new_page()
+        page.goto(URL)
+
+        for _ in range(3):
+            # Wait for the page to load (helps to load the comments on the video)
+            page.wait_for_page_ready_state()
+            # Scroll down the page to trigger loading of comments
+            page.keyboard.press("PageDown")
+
+        response = page.query_data(QUERY)
+
+        # Print the first comment
+        print(response["comments"][0])
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
