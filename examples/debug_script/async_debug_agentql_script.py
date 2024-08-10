0e7d40546085629f0ba24abd1eccac79497b8417
Tianling Feng <frank@tinyfish.io>
2024-08-10 18:30:48 -0500

    Update debug_agentql_script examples

--- examples/debug_script/async_debug_agentql_script.py
+++ examples/debug_script/async_debug_agentql_script.py
@@ -3,34 +3,42 @@
 import asyncio
 import logging
 
-import agentql
 from agentql.async_api import DebugManager
+from agentql.ext.playwright.async_api import Page
+from playwright.async_api import async_playwright
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+URL = "https://google.com"
+
+# Define the queries to interact with the page
+QUERY = """
+    {
+        search_box
+        search_btn
+        about_link
+    }
+    """
+
 
 async def main():
     # The following async context manager will enable debug mode for the script.
-    async with DebugManager.debug_mode():
-        session = await agentql.start_async_session("https://www.google.com")
+    async with DebugManager.debug_mode(), async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
 
-        # Define the queries to interact with the page
-        QUERY = """
-        {
-            search_box
-            search_btn
-            about_link
-        }
-        """
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = await browser.new_page()
 
-        response = await session.query(QUERY)
+        await page.goto(URL)
+
+        response = await page.query_elements(QUERY)
 
         # Buggy code that will crash the script. When it crashes, the debug manager will save debug files to designated directory (~/.agentql/debug by default).
-        await response.search.type("Tinyfish")
-        await response.search_btn.click()
+        await response.search.fill("tinyfish")
+        await response.search_btn.click(force=True)
 
-        await session.stop()
+        await browser.close()
 
 
 if __name__ == "__main__":
--- examples/debug_script/debug_agentql_script.py
+++ examples/debug_script/debug_agentql_script.py
@@ -2,36 +2,43 @@
 
 import logging
 
-import agentql
+from agentql.ext.playwright.sync_api import Page
 from agentql.sync_api import DebugManager
+from playwright.sync_api import sync_playwright
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+URL = "https://google.com"
 
-def debug_manager_showcase():
-    # The following context manager will enable debug mode for the script.
-    with DebugManager.debug_mode():
-        session = agentql.start_session("https://www.google.com")
+# Define the queries to interact with the page
+QUERY = """
+    {
+        search_box
+        search_btn
+        about_link
+    }
+    """
 
-        # Define the queries to interact with the page
-        QUERY = """
-            {
-                search_box
-                search_btn
-                about_link
-            }
-            """
 
-        response = session.query(QUERY)
+def main():
+    # The first context manager below will enable debug mode for the script.
+    with DebugManager.debug_mode(), sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = browser.new_page()
+
+        page.goto(URL)
+
+        response = page.query_elements(QUERY)
 
         # Buggy code that will crash the script. When it crashes, the debug manager will save debug files to designated directory (~/.agentql/debug by default).
         response.search.fill("tinyfish")
         response.search_btn.click(force=True)
 
-        session.stop()
-        log.debug(session.get_last_trail())
+        browser.close()
 
 
 if __name__ == "__main__":
-    debug_manager_showcase()
+    main()
