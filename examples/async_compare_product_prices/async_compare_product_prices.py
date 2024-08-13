089c99c4fc2565293744e8867022c4f344009a29
Tianling Feng <frank@tinyfish.io>
2024-08-13 13:44:21 -0500

    Address comments by removing table output and fixing pylance warning

--- examples/async_compare_product_prices/async_compare_product_prices.py
+++ examples/async_compare_product_prices/async_compare_product_prices.py
@@ -13,40 +13,29 @@ NINETENDO_URL = "https://www.nintendo.com/us/store/products/nintendo-switch-oled
 # Define the queries to interact with the page
 PRODUCT_INFO_QUERY = """
 {
-    nintendo_switch
-    {
-        price
-    }
-}"""
-
-
-def print_header():
-    """Prints the header for the data table"""
-    print(f"{'Website':<25} | {'Product ':<20} | {'Price ':<20} ")
-    print("-" * 75)
-
-
-def print_row(website, product, price):
-    """Prints the data row"""
-    print(f"{website:<25} | {product:<20} | {price:<20} ")
+    nintendo_switch_price
+}
+"""
 
 
 async def fetch_price(session_url, query):
     async with async_playwright() as playwright:
         browser = await playwright.chromium.launch(headless=False)
-        page: Page = await browser.new_page()
+
+        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
+        page: Page = await browser.new_page()  # type: ignore
+
         await page.goto(session_url)
 
-        # Fetch the data from the page
+        # Fetch the price data from the page
         data = await page.query_data(query)
+
         await browser.close()
-    return data["nintendo_switch"]["price"]
+
+    return data["nintendo_switch_price"]
 
 
 async def get_price_across_websites():
-
-    print_header()
-
     # Fetch prices concurrently
     bestbuy_price, nintendo_price, target_price = await asyncio.gather(
         fetch_price(BESTBUY_URL, PRODUCT_INFO_QUERY),
@@ -54,9 +43,13 @@ async def get_price_across_websites():
         fetch_price(TARGET_URL, PRODUCT_INFO_QUERY),
     )
 
-    print_row("BestBuy", "Nintendo Switch", bestbuy_price)
-    print_row("Nintendo site", "Nintendo Switch", nintendo_price)
-    print_row("Target", "Nintendo Switch", target_price)
+    print(
+        f"""
+    Price at BestBuy: ${bestbuy_price}
+    Price at Ninetendo: ${nintendo_price}
+    Pirce at Target: ${target_price}
+    """
+    )
 
 
 if __name__ == "__main__":
