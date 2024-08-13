d1367411ce4731c9ec63692d9adc0d0a1973da6f
Rachel Lee Nabors <rachelnabors@gmail.com>
2024-08-13 13:22:05 -0700

    adding GDPR compliance form example

---
+++ examples/close_cookies/close_cookies.py
@@ -0,0 +1,45 @@
+"""This example demonstrates how to close popup windows (like promotion form) with AgentQL."""
+
+import time
+
+from agentql.ext.playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
+
+# Set the URL to the desired website
+URL = "https://www.amazon.com/"
+
+QUERY = """
+{
+    cookies_form {
+        reject_btn
+    }
+}
+"""
+
+
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        # Create a new AgentQL page instance in the browser for web interactions
+        page: Page = browser.new_page()
+
+        page.goto(URL)
+
+        # Use query_elements() method to fetch the cookies dialog button from the page
+        response = page.query_elements(QUERY)
+
+        # Check if there is a cookie-rejection button on the page
+        if response.cookies_form.reject_btn != None:
+
+            # If so, click the close button to reject cookies
+            response.cookies_form.reject_btn.click()
+
+        # Wait for 5 seconds to see the browser in action
+        time.sleep(5)
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
