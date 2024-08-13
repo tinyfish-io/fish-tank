7e87f05c35a935dd7a1ef25699b46d60e3fe2f6c
Tianling Feng <frankfeng1998@gmail.com>
2024-08-13 15:21:38 -0500

    TF-1625 Update popup handling example (#34)
    * Update popup handling example
    
    * Update according to comments

---
+++ examples/close_popup/close_popup.py
@@ -0,0 +1,42 @@
+"""This example demonstrates how to close popup windows (like promotion form) with AgentQL."""
+
+import time
+
+from agentql.ext.playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
+
+# Set the URL to the desired website
+URL = "https://kinfield.com/"
+
+QUERY = """
+{
+    popup_form {
+        close_btn
+    }
+}
+"""
+
+
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
+        page: Page = browser.new_page()  # type: ignore
+
+        page.goto(URL)
+
+        # Use query_elements() method to fetch the close popup button from the page
+        response = page.query_elements(QUERY)
+
+        # Click the close button to close the popup
+        response.popup_form.close_btn.click()
+
+        # Wait for 5 seconds to see the browser in action
+        time.sleep(5)
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
