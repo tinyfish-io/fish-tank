0acfe909af53599375bf49929489ed53acb76159
Tianling Feng <frankfeng1998@gmail.com>
2024-08-14 11:35:18 -0500

    TF-1666 Update debug_agentql_script examples (#36)
    * Update debug_agentql_script examples
    
    * Update according to comments
    
    * Fix typo
    
    * Address comments by updating to scrapme
    
    * Fix pylance
    
    * Fix styles
    
    ---------
    
    Co-authored-by: R L Nabors <rachelnabors@users.noreply.github.com>

--- examples/debug_script/async_debug_agentql_script.py
+++ examples/debug_script/async_debug_agentql_script.py
@@ -3,34 +3,40 @@
 import asyncio
 import logging
 
-import agentql
 from agentql.async_api import DebugManager
+from agentql.ext.playwright.async_api import Page
+from playwright.async_api import async_playwright
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+URL = "https://scrapeme.live/shop/"
+
+# Define the queries to interact with the page
+QUERY = """
+{
+    search_products_box
+}
+"""
+
 
 async def main():
     # The following async context manager will enable debug mode for the script.
-    async with DebugManager.debug_mode():
-        session = await agentql.start_async_session("https://www.google.com")
+    async with DebugManager.debug_mode(), async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=False)
 
-        # Define the queries to interact with the page
-        QUERY = """
-        {
-            search_box
-            search_btn
-            about_link
-        }
-        """
+        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
+        page: Page = await browser.new_page()  # type: ignore
 
-        response = await session.query(QUERY)
+        await page.goto(URL)
+
+        response = await page.query_elements(QUERY)
 
         # Buggy code that will crash the script. When it crashes, the debug manager will save debug files to designated directory (~/.agentql/debug by default).
-        await response.search.type("Tinyfish")
-        await response.search_btn.click()
+        await response.search.fill("ivysaur")  # type: ignore
+        await page.keyboard.press("Enter")
 
-        await session.stop()
+        await browser.close()
 
 
 if __name__ == "__main__":
--- examples/debug_script/debug_agentql_script.py
+++ examples/debug_script/debug_agentql_script.py
@@ -2,36 +2,41 @@
 
 import logging
 
-import agentql
+from agentql.ext.playwright.sync_api import Page
 from agentql.sync_api import DebugManager
+from playwright.sync_api import sync_playwright
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
+URL = "https://scrapeme.live/shop/"
 
-def debug_manager_showcase():
-    # The following context manager will enable debug mode for the script.
-    with DebugManager.debug_mode():
-        session = agentql.start_session("https://www.google.com")
+# Define the queries to interact with the page
+QUERY = """
+{
+    search_products_box
+}
+"""
 
-        # Define the queries to interact with the page
-        QUERY = """
-            {
-                search_box
-                search_btn
-                about_link
-            }
-            """
 
-        response = session.query(QUERY)
+def main():
+    # The first context manager below will enable debug mode for the script.
+    with DebugManager.debug_mode(), sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
+        page: Page = browser.new_page()  # type: ignore
+
+        page.goto(URL)
+
+        response = page.query_elements(QUERY)
 
         # Buggy code that will crash the script. When it crashes, the debug manager will save debug files to designated directory (~/.agentql/debug by default).
-        response.search.fill("tinyfish")
-        response.search_btn.click(force=True)
+        response.search.fill("ivysaur")  # type: ignore
+        page.keyboard.press("Enter")
 
-        session.stop()
-        log.debug(session.get_last_trail())
+        browser.close()
 
 
 if __name__ == "__main__":
-    debug_manager_showcase()
+    main()
