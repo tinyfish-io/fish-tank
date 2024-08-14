4cb8b7203e68447b81f7bee0b263d4a40236ab7e
Tianling Feng <frankfeng1998@gmail.com>
2024-08-14 11:36:06 -0500

    Update XPath example (#43)

--- application_examples/xpath/xpath.py
+++ application_examples/xpath/xpath.py
@@ -1,27 +1,38 @@
-import agentql
+"""This example demonstrates how to get XPath of an element that was fetched with AgentQL."""
+
+from agentql.ext.playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
 
 # import https://pypi.org/project/playwright-dompath/
 # Playwright Dompath is a Python library that helps you to generate XPath from Playwright selectors.
 from playwright_dompath.dompath_sync import xpath_path
 
-session = agentql.start_session("https://www.google.com")
+URL = "https://scrapeme.live/shop/"
 
 QUERY = """
 {
-    search_box
-    search_btn
-    about_link
+    search_products_box
 }
 """
 
-response = session.query(QUERY)
 
-print(response.about_link.text_content())
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
 
-# Get the XPath
-print("XPath:", xpath_path(response.search_btn))
+        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
+        page: Page = browser.new_page()  # type: ignore
 
-response.search_box.fill("tinyfish")
-response.search_btn.click(force=True)
+        page.goto(URL)
 
-session.stop()
+        # Use query_elements() method to fetch the search box from the page
+        response = page.query_elements(QUERY)
+
+        # Get the XPath
+        print("XPath:", xpath_path(response.search_products_box))
+
+        browser.close()
+
+
+if __name__ == "__main__":
+    main()
