ea6c6d29ca0467991072bd7d5767acc621df291f
Tianling Feng <frank@tinyfish.io>
2024-08-16 12:24:16 -0500

    Address comments

--- examples/interact_with_external_or_existing_browser/interact_with_external_or_existing_browser.py
+++ examples/interact_with_external_or_existing_browser/interact_with_external_or_existing_browser.py
@@ -1,14 +1,14 @@
-"""This example demonstrates how to run the script in an existing local browser with AgentQL."""
+"""This example demonstrates how to interact with an external or existing browser with AgentQL."""
 
 from agentql.ext.playwright.sync_api import Page
 from playwright.sync_api import sync_playwright
 
-# The URL to connect to your local existing browser.
+# The URL of the external or existing browser you wish to connect.
 WEBSOCKET_URL = "YOUR_WEBSOCKET_URL"
 
 URL = "https://scrapeme.live/shop"
 
-QUERY = """
+VIATOR_TOURS_QUERY = """
 {
     tours[] {
         title
@@ -18,13 +18,13 @@ QUERY = """
 }
 """
 
-QUERY_FOR_SCRAPE_ME_1 = """
+SEARCH_QUERY = """
 {
     search_products_box
 }
 """
 
-QUERY_FOR_SCRAPE_ME_2 = """
+STOCK_QUERY = """
 {
     number_in_stock
 }
@@ -34,14 +34,14 @@ QUERY_FOR_SCRAPE_ME_2 = """
 def fetch_data_from_open_website_page():
     """This function demonstrates how to fetch data from open pages in your local browser."""
     with sync_playwright() as p:
-        # Connect to the existing browser via Chrome DevTools Protocol
+        # Connect to the browser via Chrome DevTools Protocol
         browser = p.chromium.connect_over_cdp(WEBSOCKET_URL)
 
         # Get the first page from the opened browser
         page: Page = browser.contexts[0].pages[0]  # type: ignore
 
-        # Use query_elements() method to locate the search box and search button from the page
-        response = page.query_data(QUERY)
+        # Use query_data() method to fetch the data from the page
+        response = page.query_data(VIATOR_TOURS_QUERY)
 
         print(response)
 
@@ -49,28 +49,25 @@ def fetch_data_from_open_website_page():
 def interact_with_new_page_in_local_browser():
     """This function demonstrates how to open and interact with a new page your local browser."""
     with sync_playwright() as p:
-        # Connect to the existing browser via Chrome DevTools Protocol
+        # Connect to the browser via Chrome DevTools Protocol
         browser = p.chromium.connect_over_cdp(WEBSOCKET_URL)
 
-        # Create a new tab in the existing browser window
+        # Create a new tab in the browser window
         page: Page = browser.contexts[0].new_page()  # type: ignore
 
         page.goto(URL)
 
         # Use query_elements() method to locate the search product box from the page
-        response = page.query_elements(QUERY_FOR_SCRAPE_ME_1)
+        response = page.query_elements(SEARCH_QUERY)
 
         # Use Playwright's API to fill the search box and press Enter
         response.search_products_box.type("Charmander")
         page.keyboard.press("Enter")
 
         # Use query_data() method to fetch the stock number from the page
-        response = page.query_data(QUERY_FOR_SCRAPE_ME_2)
+        response = page.query_data(STOCK_QUERY)
 
         print(response)
-
-        # Close the browser here will close your local browser
-        browser.close()
 
 
 if __name__ == "__main__":
