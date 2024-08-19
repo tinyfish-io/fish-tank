6340ecad37faaf8f7b013d8ea9d71dbc25bce84b
Shuhao Zhang <shuhao@tinyfish.io>
2024-08-18 23:26:42 -0700

    open multiple tab example

--- examples/compare_product_prices/async_compare_product_prices.py
+++ examples/compare_product_prices/async_compare_product_prices.py
@@ -1,9 +1,9 @@
-"""This example demonstrates how to asynchronously compare product prices across websites with query_data() method."""
+"""This example demonstrates how to asynchronously fetch product prices across websites in parallel with query_data() method."""
 
 import asyncio
 
 from agentql.ext.playwright.async_api import Page
-from playwright.async_api import async_playwright
+from playwright.async_api import BrowserContext, async_playwright
 
 # Set the URL to the desired website
 BESTBUY_URL = "https://www.bestbuy.com/site/nintendo-switch-oled-model-w-joy-con-white/6470923.p?skuId=6470923"
@@ -18,38 +18,37 @@ PRODUCT_INFO_QUERY = """
 """
 
 
-async def fetch_price(session_url, query):
-    async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=False)
+async def fetch_price(context: BrowserContext, session_url, query):
+    """Open the given URL in a new tab and fetch the price of the product."""
+    # Create a page in a new tab in the broswer context and cast it to custom Page type to get access to the AgentQL's querying API
+    page: Page = await context.new_page()  # type: ignore
 
-        # Create a new page in the broswer and cast it to custom Page type to get access to the AgentQL's querying API
-        page: Page = await browser.new_page()  # type: ignore
+    await page.goto(session_url)
 
-        await page.goto(session_url)
-
-        # Fetch the price data from the page
-        data = await page.query_data(query)
-
-        await browser.close()
-
+    # Fetch the price data from the page
+    data = await page.query_data(query)
     return data["nintendo_switch_price"]
 
 
 async def get_price_across_websites():
-    # Fetch prices concurrently
-    bestbuy_price, nintendo_price, target_price = await asyncio.gather(
-        fetch_price(BESTBUY_URL, PRODUCT_INFO_QUERY),
-        fetch_price(NINETENDO_URL, PRODUCT_INFO_QUERY),
-        fetch_price(TARGET_URL, PRODUCT_INFO_QUERY),
-    )
+    """Fetch prices concurrently in the same browser session from multiple websites."""
+    async with async_playwright() as playwright, await playwright.chromium.launch(
+        headless=False
+    ) as browser, await browser.new_context() as context:
+        # Open multiple tabs in the same browser context to fetch prices concurrently
+        bestbuy_price, nintendo_price, target_price = await asyncio.gather(
+            fetch_price(context, BESTBUY_URL, PRODUCT_INFO_QUERY),
+            fetch_price(context, NINETENDO_URL, PRODUCT_INFO_QUERY),
+            fetch_price(context, TARGET_URL, PRODUCT_INFO_QUERY),
+        )
 
-    print(
-        f"""
-    Price at BestBuy: ${bestbuy_price}
-    Price at Ninetendo: ${nintendo_price}
-    Price at Target: ${target_price}
-    """
-    )
+        print(
+            f"""
+        Price at BestBuy: ${bestbuy_price}
+        Price at Ninetendo: ${nintendo_price}
+        Price at Target: ${target_price}
+        """
+        )
 
 
 if __name__ == "__main__":
